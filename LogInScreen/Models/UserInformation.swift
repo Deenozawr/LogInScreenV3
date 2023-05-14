d17d968dfe42327f0aa93039acb24f5403162257
Deniss Akisins <Denjaa3@icloud.com>
2023-05-14 23:56:55 +0300

    Start to setup Models

--- LogInScreen/Models/UserInformation.swift
+++ LogInScreen/Models/UserInformation.swift
@@ -5,4 +5,23 @@
 //  Created by Денис on 14/05/2023.
 //
 
+struct User {
+    let login: String
+    let password: String
+    let person: Person
+    
+    static func currentUser() -> Person {
+        let person = Person(name: "John", surname: "Doe", age: 30, yearOfBirth: 1997, country: "Latvia")
+        let user = User(login: "Deniss", password: "123qwerty", person: person)
+        return person
+    }
+}
+
+struct Person {
+    let name: String
+    let surname: String
+    let age: Int
+    let yearOfBirth: Int
+    let country: String
+}
 
